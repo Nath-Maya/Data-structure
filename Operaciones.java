2a5ba7c6275e09d7e467c7d3993c9397c5619813
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-05 14:50:30 -0500

    Create Class Operations

---
+++ Operaciones.java
@@ -0,0 +1,22 @@
+public class Operaciones {
+
+    //Funcion para realizar operacion de  Suma
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    //Funcion para realizar operacion de Resta
+    public static int resta(int a, int b) {
+        return a - b;
+    }
+
+    //Funcion para realizar operacion de Multiplicacion
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    //Funcion para realizar operacion de Division
+    public static int division(int a, int b) {
+        return a / b;
+    }
+}
