90c2b68f1ad2046c6f32d489b972780ccb1d896b
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-09 22:35:44 -0500

    Add another version of execution of point 1

--- Activity 2/Point_1B/TenOrderedNumbersV2.java
+++ Activity 2/Point_1B/TenOrderedNumbersV2.java
@@ -1,34 +1,40 @@
+package Point_1B;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-//1. Programa que lea 10 números por teclado, los almacene en un array y los ordene de menor a mayor y los muestre.
-
-public class TenOrderedNumbers {
+public class TenOrderedNumbersV2 {
     public static void main(String[] args) {
+
         long startTime = System.currentTimeMillis();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Este programa ordenara de menor a mayor 10 numeros ");
 
         //Crear un array con capacidad de almacenar 10 numeros
-        int[] unorderedNumbers = new int[10];
+        int[] numbers = new int[10];
 
         System.out.println("Digite los numeros a ser ordenados :");
 
         //Bucle para Ingresar los numeros (desde teclado) e introducirlos en el array
-        for (int i = 0; i < unorderedNumbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             //Solicitud de ingreso de cada numero al array
             System.out.println("Número #" + (i + 1) + " - ");
-            unorderedNumbers[i] = sc.nextInt();
+            numbers[i] = sc.nextInt();
         }
 
         //Mostrar array antes de ordenar
-        System.out.println("Numeros ingresados (sin ordenar):" + Arrays.toString(unorderedNumbers));
+        System.out.println("Numeros ingresados (sin ordenar):" + Arrays.toString(numbers));
 
         //Metodo para ordenar los numeros
-        Arrays.sort(unorderedNumbers);
+        Arrays.sort(numbers);
+
         //Mostrar array despues de ordenar
-        System.out.println("Numeros ordenados:" + Arrays.toString(unorderedNumbers));
+        System.out.println("Numeros ordenados:" );
+        //Bucle for para imprimir los numeros ordenados
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
 
         sc.close();
         long endTime = System.currentTimeMillis();
