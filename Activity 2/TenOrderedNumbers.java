428c6a0c80fa03fbc07d41945dfb138bd2e4af9c
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-09 22:27:38 -0500

    Rename array variable to ordered Numbers and other settings

--- Activity 2/TenOrderedNumbers.java
+++ Activity 2/TenOrderedNumbers.java
@@ -3,35 +3,36 @@ import java.util.Scanner;
 
 //1. Programa que lea 10 números por teclado, los almacene en un array y los ordene de menor a mayor y los muestre.
 
-public class TenNumbers {
+public class TenOrderedNumbers {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Este programa ordenara de menor a mayor 10 numeros ");
 
-        //Crear un array con 10 posiciones
-        int[] arr = new int[10];
+        //Crear un array con capacidad de almacenar 10 numeros
+        int[] unorderedNumbers = new int[10];
 
         System.out.println("Digite los numeros a ser ordenados :");
 
-        //Bucle para Ingresar los numeros
-        for (int i = 0; i < arr.length; i++) {
+        //Bucle para Ingresar los numeros (desde teclado) e introducirlos en el array
+        for (int i = 0; i < unorderedNumbers.length; i++) {
             //Solicitud de ingreso de cada numero al array
             System.out.println("Número #" + (i + 1) + " - ");
-            arr[i] = sc.nextInt();
+            unorderedNumbers[i] = sc.nextInt();
         }
 
-        //Metodo de la clase Arrays para imprimir los elementos del array
-        System.out.println("Numeros ingresados (sin ordenar):" + Arrays.toString(arr));
+        //Mostrar array antes de ordenar
+        System.out.println("Numeros ingresados (sin ordenar):" + Arrays.toString(unorderedNumbers));
 
         //Metodo para ordenar los numeros
-        Arrays.sort(arr);
-        System.out.println("Numeros ordenados:" + Arrays.toString(arr));
+        Arrays.sort(unorderedNumbers);
+        //Mostrar array despues de ordenar
+        System.out.println("Numeros ordenados:" + Arrays.toString(unorderedNumbers));
 
         sc.close();
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
-        System.out.println("⏱ Tiempo de ejecucion: " + (totalTime / 1_000_000.0) + " ms");
+        System.out.println("⏱ Tiempo de ejecucion: " + totalTime + " ms");
     }
 }
