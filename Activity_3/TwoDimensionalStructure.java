f2fbf7b70e5091697b80d81e3705dc0349d00080
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-17 23:27:35 -0500

    Enhance user interface in TwoDimensionalStructure by adding ANSI colors and improving prompt clarity for matrix dimensions

--- Activity_3/TwoDimensionalStructure.java
+++ Activity_3/TwoDimensionalStructure.java
@@ -8,26 +8,36 @@ public class TwoDimensionalStructure {
 
         Scanner inputScanner = new Scanner(System.in);
 
-        System.out.println("PROGRAMA PARA REALIZAR OPERACIONES CON MATRICES");
+        // Colores ANSI
+        final String YELLOW_BRIGHT = "\u001B[93m";
+        final String RESET = "\u001B[0m";
 
-        // Solicitar al usuario el numero de filas y columnas para las dos matrices
-        System.out.println("Filas de la matriz 1");
+        // Encabezado
+        System.out.println("\n" + YELLOW_BRIGHT +
+                "✨──────────────────────────────────────────────✨");
+        System.out.println(" PROGRAMA PARA OPERAR CON MATRICES  " + RESET + "\n");
+
+        // Solicitar al usuario el número de filas y columnas para las dos matrices
+        System.out.println("Filas de la matriz 1:");
         int rows1 = inputScanner.nextInt();
-        System.out.println("Columnas de la matriz 1");
+        System.out.println("Columnas de la matriz 1:");
         int cols1 = inputScanner.nextInt();
-        System.out.println("Filas de la matriz 2");
+
+        System.out.println("Filas de la matriz 2:");
         int rows2 = inputScanner.nextInt();
-        System.out.println("Columnas de la matriz 2");
+        System.out.println("Columnas de la matriz 2:");
         int cols2 = inputScanner.nextInt();
 
         // Crear una instancia de GenerateMatrix
         GenerateMatrix matrixGenerator = new GenerateMatrix(rows1, cols1, rows2, cols2);
 
-        matrixGenerator.fillMatrices(inputScanner, matrixGenerator.getMatrix1(), " 1");
-        matrixGenerator.fillMatrices(inputScanner, matrixGenerator.getMatrix2(), " 2");
+        // Llenar las matrices con valores ingresados por el usuario
+        matrixGenerator.fillMatrices(inputScanner, matrixGenerator.getMatrix1(), "1");
+        matrixGenerator.fillMatrices(inputScanner, matrixGenerator.getMatrix2(), "2");
 
-        matrixGenerator.displayMatrices(matrixGenerator.getMatrix1(), " 1");
-        matrixGenerator.displayMatrices(matrixGenerator.getMatrix2(), " 2");
+        // Mostrar las matrices ingresadas por el usuario
+        matrixGenerator.displayMatrices(matrixGenerator.getMatrix1(), "1");
+        matrixGenerator.displayMatrices(matrixGenerator.getMatrix2(), "2");
 
         inputScanner.close();
     }
