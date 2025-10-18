841b61a064ee5fd0c072877176bc2fb98c4ed677
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-17 22:23:06 -0500

    Refactor GenerateMatrix and TwoDimensionalStructure classes to improve matrix handling and user input functionality

--- Activity_3/GenerateMatrix.java
+++ Activity_3/GenerateMatrix.java
@@ -1,10 +1,63 @@
- 
+package Activity_3;
+
+import java.util.Scanner;
 
 public class GenerateMatrix {
 
+    // ATRIBUTOS DE LA CLASE
     // Declarar dos matrices de enteros
-    private int[][] matrix1 = new int[][];
-    private int[][] matrix2 = new int[][];
+    private int[][] matrix1;
+    private int[][] matrix2;
 
+    // CONSTRUCTOR DE LA CLASE
+    public GenerateMatrix(int rows1, int cols1, int rows2, int cols2) {
+
+        this.matrix1 = new int[rows1][cols1];
+        this.matrix2 = new int[rows2][cols2];
+
+    }
+
+    // METODOS DE LA CLASE
+
+    // Metodo para llenar las matrices con valores ingresados por el usuario
+    public void fillMatrices(Scanner inputScanner) {
+
+        System.out.println("Ingrese los valores para la matriz 1:");
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                // Llenar matrix1
+                matrix1[i][j] = inputScanner.nextInt();
+            }
+        }
+
+        System.out.println("Ingrese los valores para la matriz 2:");
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
+                // Llenar matrix2
+                matrix2[i][j] = inputScanner.nextInt();
+            }
+        }
+
+    }
+
+    // Metodo para mostrar las matrices
+    public void displayMatrices() {
+        System.out.println("Matriz 1:");
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Matriz 2:");
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
--- Activity_3/TwoDimensionalStructure.java
+++ Activity_3/TwoDimensionalStructure.java
@@ -1,9 +1,31 @@
 package Activity_3;
 
+import java.util.Scanner;
+
 public class TwoDimensionalStructure {
 
     public static void main(String[] args) {
-        System.err.println("Two Dimensional Structure");
-    }
 
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("PROGRAMA PARA REALIZAR OPERACIONES CON MATRICES");
+
+        // Solicitar al usuario el numero de filas y columnas para las dos matrices
+        System.out.println("Filas de la matriz 1");
+        int rows1 = inputScanner.nextInt();
+        System.out.println("Columnas de la matriz 1");
+        int cols1 = inputScanner.nextInt();
+        System.out.println("Filas de la matriz 2");
+        int rows2 = inputScanner.nextInt();
+        System.out.println("Columnas de la matriz 2");
+        int cols2 = inputScanner.nextInt();
+
+        // Crear una instancia de GenerateMatrix
+        GenerateMatrix matrixGenerator = new GenerateMatrix(rows1, cols1, rows2, cols2);
+
+        matrixGenerator.fillMatrices(inputScanner);
+        matrixGenerator.displayMatrices();
+
+        inputScanner.close();
+    }
 }
