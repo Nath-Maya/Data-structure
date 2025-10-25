6154ee0bf9e33bfbb8af6be17c965e23fc96e75e
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-10-25 15:18:29 -0500

    Student class definition with attributes, constructor and getters

---
+++ Activity 4/Student.java
@@ -0,0 +1,37 @@
+package Activity 4;
+
+public class Student {
+
+    //ATRIBUTOS
+    private int identificationCode;
+    private String name;
+    private String lastName;
+    private int age;
+
+    //CONSTRUCTOR
+    public Student(int identificationCode, String name, String lastName, int age) {
+        this.identificationCode = identificationCode;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+
+    //METODOS GETTER
+    public int getIdentificationCode() {
+        return identificationCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
