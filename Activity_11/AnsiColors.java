defee9108a43d49dbd0a331b5c973a4fb4f4d041
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-11-23 23:57:42 +0100

    Add AnsiColors class for terminal color formatting with predefined color constants

---
+++ Activity_11/AnsiColors.java
@@ -0,0 +1,10 @@
+package Activity_11;
+
+
+public class AnsiColors {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";      // errores / rotaciones dobles
+    public static final String YELLOW = "\u001B[33m";   // advertencias
+    public static final String CYAN = "\u001B[36m";     // información
+    public static final String GREEN = "\u001B[32m";    // éxito / confirmaciones
+}
